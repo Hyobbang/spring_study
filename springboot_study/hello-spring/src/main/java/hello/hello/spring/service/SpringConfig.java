8edcaa5a171bc9af9bc1b55c0347889ce128feab
hyoeun <wgb1212@atg.co.kr>
2022-09-29 15:54:04 +0900

    09/29 커밋 (JPA 적용 버전 / DB는 안 붙임)

---
+++ springboot_study/hello-spring/src/main/java/hello/hello/spring/service/SpringConfig.java
@@ -0,0 +1,27 @@
+package hello.hello.spring.service;
+
+import hello.hello.spring.repository.*;
+import hello.hello.spring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+@Configuration
+public class SpringConfig { private final DataSource dataSource;
+    private final EntityManager em;
+    public SpringConfig(DataSource dataSource, EntityManager em) {
+        this.dataSource = dataSource;
+        this.em = em;
+    }
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+    @Bean
+    public MemberRepository memberRepository() {
+// return new MemoryMemberRepository();
+// return new JdbcMemberRepository(dataSource);
+// return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
+    }
+}
