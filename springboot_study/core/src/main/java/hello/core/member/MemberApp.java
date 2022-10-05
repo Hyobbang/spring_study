5b56551006c4fcfe4f76d648840095260d1db21f
hyoeun <wgb1212@atg.co.kr>
2022-10-05 18:53:02 +0900

    10/05 커밋 ~싱글톤까지

--- springboot_study/core/src/main/java/hello/core/member/MemberApp.java
+++ springboot_study/core/src/main/java/hello/core/member/MemberApp.java
@@ -1,16 +1,22 @@
 package hello.core.member;
 
-import hello.core.member.Grade;
-import hello.core.member.Member;
-import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import hello.core.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        //이렇게 하면 어플리케이션 컨테이너에서 Bean으로 집어넣었던 애들을 싹 다 관리해줌
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
         Member findMember = memberService.findMember(1L);
+
     }
 }
--- springboot_study/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ springboot_study/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,13 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    //AppConfig에서 넣어줬기 때문에 여기에서는 인터페이스만 의존하게됨
+    //생성자의 매개변수를 통해 AppConfig에서 구현체를 MemberServiceImpl에 전달함! => 생성자 주입
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     //MemberRepository의 구현체인 MemoryMemberRepository의 join 메소드가 호출이 됨
     @Override
